71c4eadab46d7dc5b169173dc8f6ba6c00689ccb
rccordell <rccordell@gmail.com>
2026-01-31 16:42:47 +0000

    Deploying to gh-pages from @ rccordell/rccordell.github.io@20a2546549cc7e75d9f688a0eb78968ce8e66de0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "My research projects in digital humanities, book history, and machine learning.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -44,28 +44,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-response-student-centered-ai-amp-dh-practices-roundtable",
+        },{id: "post-response-student-centered-ai-amp-dh-practices-roundtable",
         
           title: "Response: Student-Centered AI &amp; DH Practices Roundtable",
         
